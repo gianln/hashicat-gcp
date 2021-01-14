898e1488e0297c65381287be2f5d7e9718f73910
Lien Giang <gianln@yahoo.com>
2021-01-14 22:29:12 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "network" {
-  source  = "app.terraform.io/lien-training/network/google"
+  #source  = "app.terraform.io/lien-training/network/google"
+  source = "terraform-google-modules/network/google"
   version = "2.5.0"
   # insert required variables here
 
