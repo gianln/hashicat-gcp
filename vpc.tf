9156cb600acf44cf52de90132f83b0a8d5b17bd1
Lien Giang <gianln@yahoo.com>
2021-01-14 22:23:44 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+module "network" {
+  source  = "app.terraform.io/lien-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+
+  network_name = "lien-network"
+  project_id = var.project
+
+  subnets = [
+  {
+    subnet_name   = "lien-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
