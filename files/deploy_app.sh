1c49170d53b3f39b57c2c1c070d9d39b8e08aaf5
gianln <77300275+gianln@users.noreply.github.com>
2021-01-14 14:45:09 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Hashicap GCP Training.
+  Welcome to ${PREFIX}'s app. Hashicap GCP Training 123.
   <!-- END -->
 
   </div>
